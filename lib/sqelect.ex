a8b2c975addc2092b07b3d2ed64908b7248d3aa6
Dmitrii Dimandt <dmitrii@dmitriid.com>
2020-07-26 16:41:10 +0200

    Start from scratch

---
+++ lib/sqelect.ex
@@ -0,0 +1,18 @@
+defmodule Sqelect do
+  @moduledoc """
+  Documentation for `Sqelect`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sqelect.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sqelect_test.exs
@@ -0,0 +1,8 @@
+defmodule SqelectTest do
+  use ExUnit.Case
+  doctest Sqelect
+
+  test "greets the world" do
+    assert Sqelect.hello() == :world
+  end
+end
